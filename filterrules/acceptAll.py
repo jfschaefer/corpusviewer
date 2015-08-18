37b63d1ab15b9477c348e4ad09013d78f23367e7
jfschaefer <j.schaefer@jacobs-university.de>
2015-08-18 16:08:37 +0200

    minor changes

--- filterrules/acceptAll.py
+++ filterrules/acceptAll.py
@@ -1,4 +1,9 @@
-# This (trivial) filter accepts all instances.
+# The default filter. It accepts all instances.
+
+# The function filter will be called on all instances.
+# Instances for which it returns False will be discarded.
+
+# There are a few example filter rules in the repository.
 
 def filter(instance, interpretations):
     return True
